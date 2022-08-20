c245429b7b7774c54f629357712f7163a5af088b
Kean <keanjieden@gmail.com>
2022-08-21 00:21:50 +0800

    add reusable button UI

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,20 @@
+type ButtonProps = {
+  color?: string
+  children?: string
+  className?: string
+}
+
+const Button = ({ className, color, children }: ButtonProps) => {
+  return (
+    <button
+      type='button'
+      className={`${
+        color || 'bg-yellow-500'
+      } text-white text-sm py-2 px-4 rounded transition-all ease-in-out duration-300 font-semibold hover:bg-yellow-400 focus:ring-1 focus:outline-none focus:ring-yellow-500 ${className} `}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
