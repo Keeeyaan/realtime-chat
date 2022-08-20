afa09ab4bd7b0d87e4fe8b3aa4f4198c19b54988
Kean <keanjieden@gmail.com>
2022-08-21 00:22:24 +0800

    add header UI

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import { MenuOutlined } from '@ant-design/icons'
+
+import Button from './UI/Button'
+import SearchInput from './UI/SearchInput'
+
+const Header = () => {
+  return (
+    <>
+      <header className='container py-4 h-auto flex justify-between items-center px-6 sm:px-0 sm:mx-auto'>
+        <div className='flex items-center gap-6'>
+          <h1 className='text-xl font-bold text-white'>
+            <span className='text-yellow-500'>/</span>Chatty
+          </h1>
+          <Button className='hidden sm:block '>New chat</Button>
+        </div>
+        <div className='hidden sm:block'>
+          <SearchInput />
+        </div>
+        <MenuOutlined className='text-lg text-white sm:hidden' />
+      </header>
+    </>
+  )
+}
+
+export default Header
