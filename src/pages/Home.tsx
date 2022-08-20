448e06d7d73b085ea0c3b372f45ead20c5d30d2d
Kean <keanjieden@gmail.com>
2022-08-21 00:20:58 +0800

    add home page component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <div className='bg-[url(/images/dragon-scales.svg)] h-[390px]'>
+      <Header />
+    </div>
+  )
+}
+
+export default Home
