bec8cec7013eef9218918cc9bf0ccd6222f45712
Kean <keanjieden@gmail.com>
2022-08-22 00:52:59 +0800

    remove color props & hover focus effect

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,16 +1,13 @@
 type ButtonProps = {
-  color?: string
   children?: React.ReactNode
   className?: string
 }
 
-const Button = ({ className, color, children }: ButtonProps) => {
+const Button = ({ className, children }: ButtonProps) => {
   return (
     <button
       type='button'
-      className={`${
-        color || 'bg-yellow-500'
-      } text-white text-sm py-2 px-4 rounded transition-all ease-in-out duration-300 font-semibold hover:bg-yellow-400 focus:ring-1 focus:outline-none focus:ring-yellow-500 ${className} `}
+      className={`text-white text-sm py-2 px-4 rounded transition-all ease-in-out duration-300 font-semibold outline-none ${className} `}
     >
       {children}
     </button>
