aad068fd5c8e16de545248f19aa82c468e3912df
Kean <keanjieden@gmail.com>
2022-08-21 16:28:45 +0800

    add topic card UI

---
+++ src/components/UI/TopicCard.tsx
@@ -0,0 +1,20 @@
+type TopicCardProps = {
+  topic?: string
+  rooms?: number
+}
+
+const TopicCard = ({ topic, rooms }: TopicCardProps) => {
+  return (
+    <div className='w-[120px] text-center min-w-fit shadow-md transform transition-all ease-in-out duration-150 hover:-translate-y-1'>
+      <div className=' h-[70px] rounded-t bg-blue-600' />
+      <div className='min-h-[64px] bg-gray-700 rounded-b'>
+        <h1 className='p-2 pb-0 font-semibold text-medium text-white'>{topic}</h1>
+        <p className='pb-2 text-white font-light text-xs'>
+          <span className='font-medium text-xs'>{rooms}</span> Rooms
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default TopicCard
