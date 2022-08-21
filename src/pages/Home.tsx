cdb15c84b596adff1d7d1a47398dff4034491732
Kean <keanjieden@gmail.com>
2022-08-22 00:55:59 +0800

    leverage ChatRoomCard UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-import useWindowDimensions from '../components/hooks/useWindowDimensions'
+import useWindowDimensions from '../hooks/useWindowDimensions'
 import Header from '../components/Header'
 import TopicCard from '../components/UI/TopicCard'
+import ChatRoomCard from '../components/UI/ChatRoomCard'
 
 const dummyTopics = [
   { id: 1, topic: 'Food', rooms: 10 },
@@ -15,6 +16,79 @@ const dummyTopics = [
   { id: 8, topic: 'Minecraft', rooms: 64 },
   { id: 9, topic: 'Music', rooms: 27 },
   { id: 10, topic: 'Anime', rooms: 77 },
+]
+
+const dummyRooms = [
+  {
+    id: 1,
+    title: 'Ayaya Anime',
+    description: 'Just a bunch of weebs',
+    topics: [{ title: 'Anime' }, { title: 'Fun' }, { title: 'Weebs' }],
+    online: 10,
+  },
+  {
+    id: 2,
+    title: 'Minecraft',
+    description: 'Just a bunch of weebs',
+    topics: [{ title: 'Gaming' }, { title: 'Fun' }],
+    online: 23,
+  },
+  {
+    id: 3,
+    title: 'Ayaya Anime',
+    description: 'Just a bunch of weebs',
+    topics: [{ title: 'Anime' }, { title: 'Fun' }, { title: 'Weebs' }],
+    online: 10,
+  },
+  {
+    id: 4,
+    title: 'Ayaya Anime',
+    description: 'Just a bunch of weebs',
+    topics: [{ title: 'Anime' }, { title: 'Fun' }, { title: 'Weebs' }],
+    online: 10,
+  },
+  {
+    id: 5,
+    title: 'Ayaya Anime',
+    description: 'Just a bunch of weebs',
+    topics: [{ title: 'Anime' }, { title: 'Fun' }, { title: 'Weebs' }],
+    online: 10,
+  },
+  {
+    id: 6,
+    title: 'Ayaya Anime',
+    description: 'Just a bunch of weebs',
+    topics: [{ title: 'Anime' }, { title: 'Fun' }, { title: 'Weebs' }],
+    online: 10,
+  },
+  {
+    id: 7,
+    title: 'Ayaya Anime',
+    description: 'Just a bunch of weebs',
+    topics: [{ title: 'Anime' }, { title: 'Fun' }, { title: 'Weebs' }],
+    online: 10,
+  },
+  {
+    id: 8,
+    title: 'Ayaya Anime',
+    description: 'Just a bunch of weebs',
+    topics: [{ title: 'Anime' }, { title: 'Fun' }, { title: 'Weebs' }],
+    online: 10,
+  },
+  {
+    id: 9,
+    title: 'Ayaya Anime',
+    description: 'Just a bunch of weebs',
+    topics: [{ title: 'Anime' }, { title: 'Fun' }, { title: 'Weebs' }],
+    online: 10,
+  },
+  {
+    id: 10,
+    title: 'Ayaya Anime',
+    description: 'Just a bunch of weebs',
+    topics: [{ title: 'Anime' }, { title: 'Fun' }, { title: 'Weebs' }],
+    online: 10,
+  },
 ]
 
 const Home = () => {
@@ -31,7 +105,7 @@ const Home = () => {
       <h1 className=' text-center mx-auto mb-3 sm:text-start px-4 container font-bold text-white uppercase'>
         Explore Topics
       </h1>
-      <div className='flex justify-center gap-4 mb-6 flex-wrap'>
+      <div className='flex justify-center gap-4 mb-4 flex-wrap'>
         {dummyTopics
           .slice(
             ...(width <= 400
@@ -75,6 +149,18 @@ const Home = () => {
             Random
           </option>
         </select>
+        <div className='mt-2 flex flex-wrap gap-4 justify-center'>
+          {dummyRooms.map((room) => (
+            <React.Fragment key={room.id}>
+              <ChatRoomCard
+                title={room.title}
+                description={room.description}
+                topics={room.topics}
+                online={room.online}
+              />
+            </React.Fragment>
+          ))}
+        </div>
       </div>
     </div>
   )
