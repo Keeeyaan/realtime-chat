f4a33bb4042dce582ae93f29012767e417bd0c86
Kean <keanjieden@gmail.com>
2022-08-22 00:48:48 +0800

    set routes for home and chat page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
+import { Routes, Route } from 'react-router-dom'
+
 import Home from './pages/Home'
+import Chat from './pages/Chat'
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/chat/:id' element={<Chat />} />
+      </Routes>
     </>
   )
 }
