eace920d031ae406595bde5c881f652faec80289
Kean <keanjieden@gmail.com>
2022-08-22 00:55:07 +0800

    add cursor pointer & change color header

--- src/components/UI/TopicCard.tsx
+++ src/components/UI/TopicCard.tsx
@@ -5,8 +5,8 @@ type TopicCardProps = {
 
 const TopicCard = ({ topic, rooms }: TopicCardProps) => {
   return (
-    <div className='w-[120px] text-center min-w-fit shadow-md transform transition-all ease-in-out duration-150 hover:-translate-y-1'>
-      <div className=' h-[70px] rounded-t bg-blue-600' />
+    <div className=' cursor-pointer w-[120px] text-center min-w-fit shadow-md transform transition-all ease-in-out duration-150 hover:-translate-y-1'>
+      <div className=' h-[70px] rounded-t bg-blue-500' />
       <div className='min-h-[64px] bg-gray-700 rounded-b'>
         <h1 className='p-2 pb-0 font-semibold text-medium text-white'>{topic}</h1>
         <p className='pb-2 text-white font-light text-xs'>
