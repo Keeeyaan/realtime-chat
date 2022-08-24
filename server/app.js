d4f1ef250bd362c34ffb2c745b1ba365adbd6f57
Kean <keanjieden@gmail.com>
2022-08-24 23:55:35 +0800

    setup a server

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+
+//regular middleware
+app.use(express.json());
+
+//cors middleware
+app.use(
+  cors({
+    origin: 'http://127.0.0.1:5173',
+    credentials: true,
+  })
+);
+
+//router middleware
+app.get('/api/user', (req, res) => res.send('<h1>FK U</h1>'));
+
+export default app;
