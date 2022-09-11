1d789bbf062b593344d51014f1f56d942d8a4612
Kean <keanjieden@gmail.com>
2022-09-11 22:45:21 +0800

    add user context provider & remove strict mode

---
+++ client/src/context/UserProvider.tsx
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useState } from 'react'
+
+interface UserContextInterface {
+  username: string
+  setUsername: React.Dispatch<React.SetStateAction<string>>
+}
+
+const UserContext = createContext<UserContextInterface>({ username: '', setUsername: () => {} })
+
+const UserProvider = ({ children }: any) => {
+  const [username, setUsername] = useState('')
+
+  return <UserContext.Provider value={{ username, setUsername }}>{children}</UserContext.Provider>
+}
+
+const useUserContext = () => {
+  return useContext(UserContext)
+}
+
+export { UserProvider, useUserContext }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
+import { UserProvider } from './context/UserProvider'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <UserProvider>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </UserProvider>
+  </BrowserRouter>,
 )
