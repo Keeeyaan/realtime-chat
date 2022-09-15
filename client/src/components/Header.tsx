7ed05c35df0527f111af76538ca027eeff7d1d22
Kean <keanjieden@gmail.com>
2022-09-15 22:57:39 +0800

    add display logic profile for inputted nickname

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
-import { MenuOutlined } from '@ant-design/icons';
+import { MenuOutlined, SettingFilled } from '@ant-design/icons';
 
+import { useUserContext } from '../context/UserProvider';
 import Button from './UI/Button';
 import SearchInput from './UI/SearchInput';
 
 const Header = () => {
+  const { username } = useUserContext();
+
   return (
     <>
       <header className='container py-4 h-auto flex justify-between items-center px-6 sm:px-0 sm:mx-auto'>
@@ -16,8 +19,15 @@ const Header = () => {
             <Button className='hidden sm:block bg-green-500 hover:bg-green-600'>New room</Button>
           </Link>
         </div>
-        <div className='hidden sm:block'>
-          <SearchInput />
+        <SearchInput />
+        <div className='hidden sm:flex items-center gap-6'>
+          {username && (
+            <div className='flex items-center'>
+              <div className='h-[40px] w-[40px] bg-blue-600 rounded-[50%] mr-2' />
+              <h1 className='text-white font-bold'>{username}</h1>
+            </div>
+          )}
+          <SettingFilled className='cursor-pointer text-white' />
         </div>
         <MenuOutlined className='text-lg text-white sm:hidden' />
       </header>
