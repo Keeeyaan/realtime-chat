bcf9f251bc2b73de66c0b78ae39a26b6b6f2c6c5
Kean <keanjieden@gmail.com>
2022-09-15 22:55:54 +0800

    add context provider

--- client/src/context/UserProvider.tsx
+++ client/src/context/UserProvider.tsx
@@ -3,14 +3,28 @@ import React, { createContext, useContext, useState } from 'react';
 interface UserContextInterface {
   username: string;
   setUsername: React.Dispatch<React.SetStateAction<string>>;
+  showNicknameModal: boolean;
+  setShowNicknameModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const UserContext = createContext<UserContextInterface>({ username: '', setUsername: () => {} });
+const UserContext = createContext<UserContextInterface>({
+  username: '',
+  setUsername: () => {},
+  showNicknameModal: true,
+  setShowNicknameModal: () => {},
+});
 
 const UserProvider = ({ children }: any) => {
   const [username, setUsername] = useState('');
+  const [showNicknameModal, setShowNicknameModal] = useState(true);
 
-  return <UserContext.Provider value={{ username, setUsername }}>{children}</UserContext.Provider>;
+  return (
+    <UserContext.Provider
+      value={{ username, setUsername, showNicknameModal, setShowNicknameModal }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 const useUserContext = () => {
