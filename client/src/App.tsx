98e067d71bc702826de703a11349c1cf3c7560aa
Kean <keanjieden@gmail.com>
2022-09-15 22:57:03 +0800

    add display logic for nickname modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 
+import { useUserContext } from './context/UserProvider';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Room from './pages/Room';
 import CreateRoom from './pages/CreateRoom';
+import NicknameOverlay from './components/NicknameOverlay';
 
 const App = () => {
+  const { showNicknameModal } = useUserContext();
+
   return (
     <>
+      {!showNicknameModal && <NicknameOverlay />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/room/:id' element={<Room />} />
