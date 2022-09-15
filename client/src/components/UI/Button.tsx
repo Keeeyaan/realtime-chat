eb535a8c90a8eef3e6a93064e6c616de54182308
Kean <keanjieden@gmail.com>
2022-09-15 22:56:30 +0800

    add custom button onclick prop

--- client/src/components/UI/Button.tsx
+++ client/src/components/UI/Button.tsx
@@ -1,12 +1,15 @@
 type ButtonProps = {
   children?: React.ReactNode;
+  type?: 'button' | 'submit' | 'reset';
   className?: string;
+  onClick?: () => void;
 };
 
-const Button = ({ className, children }: ButtonProps) => {
+const Button = ({ className, children, type: btnType, onClick: clickTriggered }: ButtonProps) => {
   return (
     <button
-      type='button'
+      onClick={clickTriggered}
+      type={btnType}
       className={`text-white text-sm py-2 px-4 rounded transition-all ease-in-out duration-300 font-semibold outline-none ${className} `}
     >
       {children}
